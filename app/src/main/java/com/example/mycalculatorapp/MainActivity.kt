56814b2791ff59a7e91041d5e4185dfafe3523f0
Niranjan V <rathinamniranjan@gmail.com>
2023-08-28 18:18:11 +0530

    Button Digit funtionalities Completed

--- app/src/main/java/com/example/mycalculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/mycalculatorapp/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.mycalculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.example.mycalculatorapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tvAnswer:TextView
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        tvAnswer = findViewById(R.id.tvAnswer)
+
+
+    }
+
+    fun onClickDigit(view: View){
+
+        if(binding.tvAnswer.text.toString() == "0"){
+            binding.tvAnswer.text = (view as Button).text
+        }else{
+            tvAnswer.append((view as Button).text)
+        }
+
+    }
+
+    fun onClickCancel(view: View){
+        binding.tvAnswer.text = binding.tvAnswer.text.toString().substring(0,binding.tvAnswer.text.length-1)
+        if (binding.tvAnswer.text.toString() == ""){
+            binding.tvAnswer.text = "0"
+        }
+    }
+
+    fun onClear(view: View){
+        binding.tvAnswer.text = "0"
     }
 }
